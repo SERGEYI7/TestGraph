af66e74e0da7c24a825c6c603ba52bf43982a4d8
Сергей А. Полещук <PoleshchukSA@niitkd.local>
2025-04-22 14:48:10 +0600

    Добавил дополнительные проверочные условия. Убрал лишние логи

--- undocommands.cpp
+++ undocommands.cpp
@@ -13,11 +13,22 @@ void UndoCommandAddSin::make_graph() const {
 void UndoCommandAddSin::step_back() {
     if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
         undoCmd->make_graph();
+    else {
+        qDebug() << "Неизвестный тип";
+    }
 }
 
 void UndoCommandAddSin::step_forward() {
+    if (_undoStack->count() != _undoStack->index() || _undoStack->count() == 0) {
+        make_graph();
+        return;
+    }
+
     if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
         undoCmd->make_graph();
+    else {
+        qDebug() << "Неизвестный тип";
+    }
 }
 
 void UndoCommandAddSin::undo() {
@@ -25,34 +36,38 @@ void UndoCommandAddSin::undo() {
     qDebug() << "undo index in undocommand" << _undoStack->index();
     qDebug() << "amplitude = " << get_amplitude();
     qDebug() << "frequency = " << get_frequency();
-    if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
-        undoCmd->make_graph();
-    else {
-        qDebug() << "Неизвестный тип";
-    }
+    // if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
+    //     undoCmd->make_graph();
+    // else {
+    //     qDebug() << "Неизвестный тип";
+    // }
+    step_back();
 
 }
 
 void UndoCommandAddSin::redo() {
-    if (_undoStack->count() != _undoStack->index()) {
-        _qcp->make_sin(get_amplitude(), get_frequency());
-        return;
-    }
-    else if (_undoStack->count() == 0) {
-        _qcp->make_sin(get_amplitude(), get_frequency());
-        return;
-    }
+    // if (_undoStack->count() != _undoStack->index()) {
+    //     _qcp->make_sin(get_amplitude(), get_frequency());
+    //     return;
+    // }
+    // else if (_undoStack->count() == 0) {
+    //     _qcp->make_sin(get_amplitude(), get_frequency());
+    //     return;
+    // }
 
-    if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
-        undoCmd->make_graph();
-    else {
-        qDebug() << "Неизвестный тип";
-    }
+    // if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
+    //     undoCmd->make_graph();
+    // else {
+    //     qDebug() << "Неизвестный тип";
+    // }
+    step_forward();
 }
 
 UndoCommandAddParabola::UndoCommandAddParabola(CustomGraph* qcp, int cx, const QUndoStack* undoStack, UndoMyBase * parent):
     UndoMyBase(parent), _cx(cx), _qcp(qcp), _undoStack(undoStack)
-{}
+{
+    _qcp->makeDefaultGraph(get_cx());
+}
 
 void UndoCommandAddParabola::make_graph() const {
     _qcp->makeDefaultGraph(get_cx());
@@ -61,36 +76,15 @@ void UndoCommandAddParabola::make_graph() const {
 void UndoCommandAddParabola::step_back() {
     if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
         undoCmd->make_graph();
+    else {
+        qDebug() << "Неизвестный тип";
+    }
 }
 
 void UndoCommandAddParabola::step_forward() {
-    if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
-        undoCmd->make_graph();
-}
-
-void UndoCommandAddParabola::undo() {
-    qDebug() << "undo Parabola";
-    qDebug() << "undo index in undocommand" << _undoStack->index();
-    qDebug() << "cx = " << get_cx();
-    qDebug() << "undostack count" << _undoStack->count();
-    if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
-        undoCmd->make_graph();
-    else {
-        qDebug() << "Не известный тип";
-    }
-}
-
-void UndoCommandAddParabola::redo() {
-    qDebug() << "redo Parabola";
-    qDebug() << "redo Parabola count = " << _undoStack->count();
-    qDebug() << "redo parabola in undo command parabola " << _undoStack->index();
-    if (_undoStack->count() != _undoStack->index()) {
-        _qcp->makeDefaultGraph(get_cx());
+    if (_undoStack->count() != _undoStack->index() || _undoStack->count() == 0) {
+        make_graph();
         return;
-    }
-    else if (_undoStack->count() == 0) {
-            _qcp->makeDefaultGraph(get_cx());
-            return;
     }
 
     if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
@@ -98,4 +92,38 @@ void UndoCommandAddParabola::redo() {
     else {
         qDebug() << "Неизвестный тип";
     }
+}
+
+void UndoCommandAddParabola::undo() {
+    qDebug() << "undo Parabola";
+    qDebug() << "undo index in undocommand" << _undoStack->index();
+    qDebug() << "cx = " << get_cx();
+    qDebug() << "undostack count" << _undoStack->count();
+    // if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index()-2)))
+    //     undoCmd->make_graph();
+    // else {
+    //     qDebug() << "Не известный тип";
+    // }
+    step_back();
+}
+
+void UndoCommandAddParabola::redo() {
+    qDebug() << "redo Parabola";
+    qDebug() << "redo Parabola count = " << _undoStack->count();
+    qDebug() << "redo parabola in undo command parabola " << _undoStack->index();
+    // if (_undoStack->count() != _undoStack->index()) {
+    //     _qcp->makeDefaultGraph(get_cx());
+    //     return;
+    // }
+    // else if (_undoStack->count() == 0) {
+    //         _qcp->makeDefaultGraph(get_cx());
+    //         return;
+    // }
+
+    // if (const UndoMyBase * undoCmd = dynamic_cast<const UndoMyBase*>(_undoStack->command(_undoStack->index())))
+    //     undoCmd->make_graph();
+    // else {
+    //     qDebug() << "Неизвестный тип";
+    // }
+    step_forward();
 }
